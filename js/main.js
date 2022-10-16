bd3ca729a01d93025ca85a0f1f5ce963037dde27
henriquepablo <pablo269704@gmail.com>
2022-10-16 10:31:09 -0300

    mudando a pasta do js e começando a implementação da lógica no conversor

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('#button').addEventListener('click', pegarDados);
+
+function pegarDados() {
+    const camp1 = document.querySelector('#options1').value;
+
+    const camp2 = document.querySelector('#options2').value;
+
+    console.log(camp1, camp2)
+}
+
+
+
